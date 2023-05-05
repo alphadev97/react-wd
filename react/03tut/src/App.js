1c4bcfb33a4782552eb531e7d102158ca4246bf6
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-05-05 23:03:30 +0500

    initial

--- react/03tut/src/App.js
+++ react/03tut/src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 function App() {
-  const handleNameChange = () => {
-    const names = ["Lee", "Job", "Arun", "Babar"];
-    const int = Math.floor(Math.random() * 4);
-    return names[int];
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello {handleNameChange()}!</p>
-
-        {/* <p>{"Alpha"}</p>
-        <p>{[1, 2, 3]}</p>
-        <p>{name}</p> */}
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
